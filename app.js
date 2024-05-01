14fe78f9275a74f6a95989cdca4061b8dbcbe551
Mayank Bhardwaj <mayank.bhardwaj@eisenvault.com>
2024-05-01 21:23:13 +0530

    Environemnt for 'Render' website hosting express

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5007;
+const port = process.env.PORT || 3001;
 const cookieParser = require("cookie-parser");
 const DefaultData = require("./defaultdata");
 require("./db/conn");
 const router = require("./routes/router");
 const products = require("./models/productsSchema");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
+// Enable CORS middleware
+
+// app.use(cors());
+
+const corsOptions = {
+  origin: "https://ecommerce-website-vert-xi.vercel.app/",
+  methods: "GET,POST",
+  credentials: true, // Enable cookies across domains
+};
+app.use(cors(corsOptions));
 
 // middleware
 app.use(express.json());
@@ -20,13 +31,10 @@ app.use(router);
 //     res.send("your server is running");
 // });
 
-
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
 }
 
-app.listen(port,()=>{
-    console.log(`your server is running on port ${port} `);
+app.listen(port, () => {
+  console.log(`your server is running on port ${port} `);
 });
-
-DefaultData();
